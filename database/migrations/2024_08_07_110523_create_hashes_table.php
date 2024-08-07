0cde69aea4c24f56d7602447bb2fe7f55b4b6f46
cy6erninja <cy6erninja@gmail.com>
2024-08-07 18:57:22 +0200

    Add a migration for hashes.

---
+++ database/migrations/2024_08_07_110523_create_hashes_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('hashes', function (Blueprint $table) {
+            $table->id();
+            $table->string('data');
+            $table->string('data_hash');
+            $table->timestamps();
+            $table->index('data_hash');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('hashes');
+    }
+};
