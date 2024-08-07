4a2e3c9f00eedcf4ab77b196cb712747ca39d01e
cy6erninja <cy6erninja@gmail.com>
2024-08-07 19:00:08 +0200

    Add hash routes.

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HashController;
 
-Route::get('/hash/{hash}', [HashController::class, 'show']);
+Route::get('/hash/{hash}', [HashController::class, 'read']);
 Route::post('/hash', [HashController::class, 'store']);
